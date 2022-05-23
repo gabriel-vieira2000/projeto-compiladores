b9f74df5937b1c6b9078801ef8ab680ac6175f5c
Gabriel Vieira Cardoso <gvc20002016@gmail.com>
2022-05-22 22:06:36 -0300

    Commit Inicial do Compilador

--- main.py
+++ main.py
@@ -3,26 +3,32 @@ import re
 import ply.lex as lex
 import PySimpleGUI as sg
 
+#Variável de Controle do Token Invalido
+g_token_invalido = ""
 
+###-------#####
+# Classe do Analisador Léxico
+###-------#####
 def AnalisadorLexico():
+
     #Lista de Tokens
     tokens = ('IFSULDEMINAS','TIPO_VAR', 'VAR', 'VAR_SENSOR', 'OPER_RELA', 'OPER_MATEMATICO',
             'OPER_ATRIB','OPER_LOGICO', 'COND_SE', 'COND_SENAO', 'COND_SENAOSE', 'REP_PARA',
             'REP_ENQUANTO', 'TENTAR', 'CASO_ERRO', 'NOME_FUNCAO', 'DEFINE_FUNCAO', 'VALOR_LOGICO',
             'VALOR_TEXTO','VALOR_INTEIRO', 'VALOR_REAL', 'VALOR_PORCENTAGEM', 'VALOR_TEMPERATURA',
             'VALOR_DATA','VALOR_HORARIO', 'ABRE_PARENT', 'FECHA_PARENT', 'ABRE_CHAVES', 'FECHA_CHAVES',
-            'ABRE_COMENTARIO', 'FECHA_COMENTARIO', 'FINAL_LINHA', 'VIRGULA')
+            'ABRE_COMENTARIO', 'FECHA_COMENTARIO', 'PONTO_FINAL', 'VIRGULA')
 
     # REGEX DE CADA TOKEN
     t_IFSULDEMINAS = r'IFSULDEMINAS'
     t_TIPO_VAR = r'~numero_inteiro | ~texto | ~porcentagem | ~temperatura | ~numero_real | ~booleano | ~lista | ~tempo | ~data'
     t_VAR = r'\#[A-z0-9\_]+'
     t_VAR_SENSOR = r'VELOCIDADE | NIVEL_TANQUE | MARCHA | TEMPERATURA | DISTANCIA_TRAS | DISTANCIA_FRENTE | POSICAO_VIDROS | VIDA_UTIL_PNEUS | ACELERADOR | FREIO | EMBREAGEM | DATA_ATUAL | HORA_ATUAL | POSICAO_DIRECAO'
-    t_OPER_RELA = r'< | > | <= | >= | = | !='
+    t_OPER_RELA = r'<= | >= | < | > |  = | !='
     t_OPER_MATEMATICO = r'\+|-|\/|\*'
     t_OPER_ATRIB = r':='
-    t_OPER_LOGICO = r'\sE\s | \sOU\s'
-    t_COND_SE = r'se'
+    t_OPER_LOGICO = r'\bE\b| \bOU\b'
+    t_COND_SE = r'\bse\b'
     t_COND_SENAO = r'senao'
     t_COND_SENAOSE = r'senaose'
     t_REP_PARA = r'para'
@@ -45,15 +51,21 @@ def AnalisadorLexico():
     t_FECHA_CHAVES = r'\}'
     t_ABRE_COMENTARIO = r'\['
     t_FECHA_COMENTARIO = r'\]'
-    t_FINAL_LINHA = r'\.'
+    t_PONTO_FINAL = r'\.'
     t_VIRGULA = r','
 
     # TOKENS que devem ser ignorados
     t_ignore = ' \t'
 
-    def t_error(t):
-        print(f'Caractere Proibido na linguagem: {t.value[0]}')
-        t.lexer.skip(1)
+    def constroiTokenInvalido(token):
+        global g_token_invalido
+        g_token_invalido = g_token_invalido + token
+        print(g_token_invalido)
+        
+    def t_error(token):
+        #print(f"Erro Encontrado! - {token.value[0]} - {token.lexer.lineno}")
+        constroiTokenInvalido(token.value[0])
+        token.lexer.skip(1)
 
     def t_newline(t):
         r'\n+'
@@ -61,11 +73,30 @@ def AnalisadorLexico():
 
     return lex.lex()
 
+
+###-------#####
+# Função para Tratar Tokens Inválidos
+###-------#####
+def verificaErroTokenInvalido(token_invalido):
+    if g_token_invalido != '':
+        if (g_token_invalido[0] == '"' and g_token_invalido[-1] != '"') or (g_token_invalido[0] != '"' and g_token_invalido[-1] == '"'):
+            return (f"Erro Léxico! - String mal formada - Token: {g_token_invalido}")
+
+        elif g_token_invalido[0] in ('1','2','3','4','5','6','7','8','9'):
+            return (f"Erro Léxico! - Número não identificado - Linha: {token.lexer.lineno} - Token: {g_token_invalido}")
+
+        else:
+            return (f"Caractere ou sequência de caracteres não reconhecido(s) na linguagem: {g_token_invalido}")
+
+
+###-------#####
+# Interface Gráfica
+###-------#####
 sg.ChangeLookAndFeel("Dark")  # Mudança do Tema
 
 WIN_W = 90
 WIN_H = 25
-filename = None
+arquivo = None
 
 file_new = "Novo        (CTRL+N)"
 file_open = "Abrir      (CTRL+O)"
@@ -74,6 +105,7 @@ file_save = "Salvar      (CTRL+S)"
 sg.Text()
 menu_layout = (
     ["Arquivo", [file_new, file_open, file_save, "---", "Sair"]],
+    ["Tabela de Tokens", ["Consultar Tabela"]],
     ["Sobre", ["Autores"]],
 )
 
@@ -85,7 +117,7 @@ layout = [
         )
     ],
     [sg.Button('Executar'), sg.Button('Limpar')],
-    [sg.Output(size=(WIN_W, WIN_H * 0.2), key='_SAIDA_')]
+    [sg.Output(font=("Consolas", 12) ,size=(WIN_W, WIN_H * 0.2), key='_SAIDA_')]
 ]
 
 window = sg.Window(
@@ -94,53 +126,54 @@ window = sg.Window(
     margins=(0, 0),
     resizable=True,
     return_keyboard_events=True,
-    icon="/moonlight_notepad/devaprender.ico",
 )
 window.read(timeout=1)
 
 window["_ENTRADA_"].expand(expand_x=True, expand_y=True)
+window["_SAIDA_"].expand(expand_x=True, expand_y=True)
 
 
-def new_file() -> str:
+def novo_arquivo() -> str:
     window["_ENTRADA_"].update(value="")
-    filename = None
-    return filename
+    arquivo = None
+    return arquivo
 
 
-def open_file() -> str:
+def abrir_arquivo() -> str:
     try:
-        filename: str = sg.popup_get_file("Abrir Arquivo", no_window=True)
+        arquivo: str = sg.popup_get_file("Abrir Arquivo", no_window=True)
     except:
         return "Erro"
-    if filename not in (None, "") and not isinstance(filename, tuple):
-        with open(filename, "r") as f:
+    if arquivo not in (None, "") and not isinstance(arquivo, tuple):
+        with open(arquivo, "r") as f:
             window["_ENTRADA_"].update(value=f.read())
-    return filename
+    return arquivo
 
 
-def save_file(filename: str):
-    if filename not in (None, ""):
-        with open(filename, "w") as f:
+def salvar_arquivo(arquivo: str):
+    if arquivo not in (None, ""):
+        with open(arquivo, "w") as f:
             f.write(values.get("_ENTRADA_"))
     else:
-        save_file_as()
+        salvar_como()
 
 
-def save_file_as() -> str:
+def salvar_como() -> str:
     try:
-        filename: str = sg.popup_get_file(
-            "Save File",
+        arquivo: str = sg.popup_get_file(
+            "Salvar Arquivo",
             save_as=True,
             no_window=True,
-            default_extension=".txt",
+            default_extension=".jtx",
             file_types=(("Text", ".txt"),),
         )
     except:
         return
-    if filename not in (None, "") and not isinstance(filename, tuple):
-        with open(filename, "w") as f:
+    
+    if arquivo not in (None, "") and not isinstance(arquivo, tuple):
+        with open(arquivo, "w") as f:
             f.write(values.get("_ENTRADA_"))
-    return filename
+    return arquivo
 
 def limpar():
     window["__ENTRADA__"].update('')
@@ -150,25 +183,74 @@ def exibir_autores():
         """
         Gabriel Vieira Cardoso
         Matheus Ribeiro da Silva
+        Trabalho Desenvolvido para a disciplina de compiladores!
         """
     )
 
+def exibir_tabela_tokens():
+    cabecalho_tabela = ["NOME_TOKEN", "LEXEMA", "EXPRESSÃO REGULAR", "DESCRIÇÃO"]
+    tokens_registrados = [
+        ["IFSULDEMINAS", "IFSULDEMINAS", "IFSULDEMINAS", "Palavra reservada IFSULDEMINAS."],
+        ["VALOR_INTEIRO", "1, 123, 1919", "VELOCIDADE | NIVEL_TANQUE | MARCHA | TEMPERATURA | DISTANCIA_TRAS | DISTANCIA_FRENTE | POSICAO_VIDROS | VIDA_UTIL_PNEUS | ACELERADOR | FREIO | EMBREAGEM | DATA_ATUAL | HORA_ATUAL | POSICAO_DIRECAO", "Valores Inteiros da Linguagem"]    
+    ]
 
+    layout = [[sg.Table(
+        values=tokens_registrados,
+        headings=cabecalho_tabela,
+        auto_size_columns=True,
+        max_col_width=50,
+        justification='center',
+        num_rows=2,
+        key="_TABELATOKENS_",
+        row_height=15
+    )]]
+    janela = sg.Window("Tabela de Tokens", layout=layout, resizable=True)
+    #block_focus(janela)
+    event, values = janela.read()
+    janela.close()
+    return None
+
+
+###-------#####
+# Função principal que executa o Analisador Léxico quando o evento "Executar" ocorre
+###-------#####
 def executar():
-    print("Criando o Analisador Léxico....")
+    global g_token_invalido
+    g_token_invalido = ""
+
+    window["_SAIDA_"].update('') 
+
     analisadorLexico = AnalisadorLexico()
-    print("Criando o Analisador Léxico....OK")
-    print("Inserindo a Entrada no Analisador Léxico....")
     analisadorLexico.input(values.get("_ENTRADA_"))
-    print("Inserindo a Entrada no Analisador Léxico....OK")
-    print("Iniciando o processo de Tokenizacao....")
+
     while True:
         token = analisadorLexico.token()
-        if not token: 
-            break      
-        print(f'Linha: {token.lineno} - Coluna: {token.lexpos} - Token: <{token.type},{token.value}>')
+        
+        if not token:
+            break 
+        
+        elif token.type == "VALOR_INTEIRO" and len(token.value) > 20:
+            print(f"Erro - Número excede o tamanho permitido (20 caracteres)! - Linha: {token.lineno} - Valor inserido: {token.value}")      
+        
+        elif token.type == "VAR" and len(token.value) > 20:
+            print(f"Erro - Nome da Variável excede o tamanho máximo permitido (20 caracteres)! - Linha: {token.lineno} - Valor inserido: {token.value}")      
+        
+        elif g_token_invalido != '':
+            verificaErroTokenInvalido(g_token_invalido)
+            g_token_invalido = ''
+        
+        else:
+            print(f'Linha: {token.lineno} - Coluna: {token.lexpos} - Token: <{token.type},{token.value}>')
+    
+    if g_token_invalido != '':
+        verificaErroTokenInvalido(g_token_invalido)
+        g_token_invalido = ''
+
     print("Processo de Análise Léxica Concluído com Sucesso")
 
+###-------#####
+# Definindo as chamadas de funções a partir dos eventos na interface (clique em botões e no menu)
+###-------#####
 while True:
     event, values = window.read()
 
@@ -176,13 +258,15 @@ while True:
         window.close()
         break
     if event in (file_new, "n:78"):
-        filename = new_file()
+        arquivo = novo_arquivo()
     if event in (file_open, "o:79"):
-        filename = open_file()
+        arquivo = abrir_arquivo()
     if event in (file_save, "s:83"):
-        save_file(filename)
+        salvar_arquivo(arquivo)
     if event in ("Save As",):
-        filename = save_file_as()
+        arquivo = salvar_como()
+    if event == "Consultar Tabela":
+        exibir_tabela_tokens()
     if event == "Autores":
         exibir_autores()
     if event == "Executar":
