87c7e0ffad9c58e0f255a62879f2c029c91419bd
Gabriel Vieira Cardoso <gvc20002016@gmail.com>
2022-05-24 18:54:32 -0300

    Ajustando detalhes finais do Analisador Lexico

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def AnalisadorLexico():
     tokens = ('IFSULDEMINAS','TIPO_VAR', 'VAR', 'VAR_SENSOR', 'OPER_RELA', 'OPER_MATEMATICO',
             'OPER_ATRIB','OPER_LOGICO', 'COND_SE', 'COND_SENAO', 'COND_SENAOSE', 'REP_PARA',
             'REP_ENQUANTO', 'TENTAR', 'CASO_ERRO', 'NOME_FUNCAO', 'DEFINE_FUNCAO', 'VALOR_LOGICO',
-            'VALOR_TEXTO','VALOR_INTEIRO', 'VALOR_REAL', 'VALOR_PORCENTAGEM', 'VALOR_TEMPERATURA',
-            'VALOR_DATA','VALOR_HORARIO', 'ABRE_PARENT', 'FECHA_PARENT', 'ABRE_CHAVES', 'FECHA_CHAVES',
-            'ABRE_COMENTARIO', 'FECHA_COMENTARIO', 'PONTO_FINAL', 'VIRGULA', 'ESPACO')
+            'VALOR_TEXTO','VALOR_INTEIRO', 'VALOR_REAL', 'VALOR_DATA','VALOR_HORARIO',
+            'ABRE_PARENT', 'FECHA_PARENT', 'ABRE_CHAVES', 'FECHA_CHAVES','ABRE_COMENTARIO',
+            'FECHA_COMENTARIO', 'PONTO_FINAL', 'VIRGULA', 'ESPACO')
 
     # REGEX DE CADA TOKEN
     t_IFSULDEMINAS = r'IFSULDEMINAS'
@@ -39,10 +39,8 @@ def AnalisadorLexico():
     t_DEFINE_FUNCAO = r'define_funcao'
     t_VALOR_LOGICO = r'Verdadeiro|Falso'
     t_VALOR_TEXTO = r'"[a-zA-Z0-9áàâãéèêíïóôõöúçÁÀÂÃÉÈÍÏÓÔÕÖÚ\.\s\_,\?!@\#$%&\|\*\(\)]*"'
-    t_VALOR_INTEIRO = r'\b-?(\d+)\b'
-    t_VALOR_REAL = r'-?(\d+)\.(\d+)'
-    t_VALOR_PORCENTAGEM = r'/(\d+(\.\d+)?%)'
-    t_VALOR_TEMPERATURA = r'-?(\d+)\.(\d+)C'
+    t_VALOR_INTEIRO = r'\b(\d+)\b'
+    t_VALOR_REAL = r'(\d+)\.(\d+)'
     t_VALOR_DATA = r'[0-3][0-9]\/[0-1][0-9]\/[0-9][0-9][0-9][0-9]'
     t_VALOR_HORARIO = r'[0-9][0-9]:[0-9][0-9]'
     t_ABRE_PARENT = r'\('
@@ -79,13 +77,13 @@ def AnalisadorLexico():
 def verificaErroTokenInvalido(token_invalido):
     if g_token_invalido != '':
         if (g_token_invalido[0] == '"' and g_token_invalido[-1] != '"') or (g_token_invalido[0] != '"' and g_token_invalido[-1] == '"'):
-            return (f"Erro Léxico! - String mal formada - Token: {g_token_invalido}")
+            return (f"String mal formada - Token: {g_token_invalido}")
 
         elif g_token_invalido[0] in ('1','2','3','4','5','6','7','8','9'):
-            return (f"Erro Léxico! - Número não identificado! Token: {g_token_invalido}")
+            return (f"Número não identificado! Token: {g_token_invalido}")
 
         else:
-            return (f"Erro Léxico! Caractere ou sequência de caracteres não reconhecido(s) na linguagem: {g_token_invalido}")
+            return (f"Caractere ou sequência de caracteres não reconhecido(s) na linguagem: {g_token_invalido}")
 
 
 ###-------#####
@@ -97,13 +95,13 @@ WIN_W = 90
 WIN_H = 25
 arquivo = None
 
-file_new = "Novo        (CTRL+N)"
-file_open = "Abrir      (CTRL+O)"
-file_save = "Salvar      (CTRL+S)"
+menu_novo_arquivo = "Novo        "
+menu_abrir_arquivo = "Abrir      "
+menu_salvar_arquivo = "Salvar      "
 
 sg.Text()
 menu_layout = (
-    ["Arquivo", [file_new, file_open, file_save, "---", "Sair"]],
+    ["Arquivo", [menu_novo_arquivo, menu_abrir_arquivo, menu_salvar_arquivo]],
     ["Tabela de Tokens", ["Consultar Tabela"]],
     ["Sobre", ["Autores"]],
 )
@@ -164,7 +162,7 @@ def salvar_como() -> str:
             save_as=True,
             no_window=True,
             default_extension=".jtx",
-            file_types=(("Text", ".txt"),),
+            file_types=(('ALL Files', '*.* *'),),
         )
     except:
         return
@@ -180,9 +178,10 @@ def limpar():
 def exibir_autores():
     sg.PopupNoTitlebar(
         """
+        Autores:
         Gabriel Vieira Cardoso
         Matheus Ribeiro da Silva
-        Trabalho Desenvolvido para a disciplina de compiladores!
+        Disciplina: Compiladores
         """
     )
 
@@ -190,18 +189,48 @@ def exibir_tabela_tokens():
     cabecalho_tabela = ["NOME_TOKEN", "LEXEMA", "EXPRESSÃO REGULAR", "DESCRIÇÃO"]
     tokens_registrados = [
         ["IFSULDEMINAS", "IFSULDEMINAS", "IFSULDEMINAS", "Palavra reservada IFSULDEMINAS."],
-        ["VALOR_INTEIRO", "1, 123, 1919", "VELOCIDADE | NIVEL_TANQUE | MARCHA | TEMPERATURA | DISTANCIA_TRAS | DISTANCIA_FRENTE | POSICAO_VIDROS | VIDA_UTIL_PNEUS | ACELERADOR | FREIO | EMBREAGEM | DATA_ATUAL | HORA_ATUAL | POSICAO_DIRECAO", "Valores Inteiros da Linguagem"]    
+        ["tipo_var", "~numero_inteiro, ~texto, ~porcentagem, ~temperatura, ~numero_real, ~booleano, ~lista, ~tempo, ~data", "~numero_inteiro | ~texto | ~porcentagem | ~temperatura | ~numero_real | ~booleano | ~lista | ~tempo | ~data", "Tipos de variáveis que a linguagem poderá aceitar."],
+        ["var", "#velocidade_anterior, #nome_condutor, #marcha2, etc.","\#[A-z0-9\_]+","Variável quando declarada ou invocada."],
+        ["var_sensor", "NIVEL_TANQUE, MARCHA, TEMPERATURA, VELOCIDADE, DISTANCIA_TRAS, DISTANCIA_FRENTE, POSICAO_VIDROS, VIDA_UTIL_PNEUS, ACELERADOR, FREIO, EMBREAGEM, DATA_ATUAL e HORA_ATUAL, POSICAO_DIRECAO", "VELOCIDADE | NIVEL_TANQUE | MARCHA | TEMPERATURA | DISTANCIA_TRAS | DISTANCIA_FRENTE | POSICAO_VIDROS | VIDA_UTIL_PNEUS | ACELERADOR | FREIO | EMBREAGEM | DATA_ATUAL | HORA_ATUAL | POSICAO_DIRECAO", "Palavras reservadas que conterão valores lidos nos sensores do carro."],
+        ["oper_rela", "<, >, <=, >=, =, !=", "<= | >= | < | > |  = | !=", "Operadores que permitem a comparação e relação entre variáveis."],
+        ["oper_matematico", '+, -, /, *', '\+|-|\/|\*', "Operadores para realizar cálculos matemáticos."],
+        ["oper_atrib", ":=", ":=", "Operadores que permitem a atribuição de um valor a uma variável ou de uma variável a outra."],
+        ["oper_logico", "E, OU", "E|OU", "Operadores lógicos para concatenar comparações."],
+        ["cond_se","se","se","Palavra reservada se para estrutura condicional: se."],
+        ["cond_senao", "senao", "senao", "Palavra reservada para estrutura condicional: senao."],
+        ["cond_senaose", "cond_senaose", "cond_senaose", "Palavra reservada para estrutura condicional: senaose."],
+        ["rep_para","para","para","Palavra reservada para estrutura de repetição: para."],
+        ["rep_enquanto", "enquanto", "enquanto", "Palavra reservada para estrutura de repetição: enquanto."],
+        ["tentar","tentar","tentar","Palavra reservada para tratamento de erros: tentar."],
+        ["caso_erro", "caso_erro", "caso_erro", "Palavra reservada para tratamento de erros: caso_erro"],
+        ["nome_funcao","!exibeMensagemPainel, !trocaMarcha, !acelera, !desacelera, etc.","!([a-zA-Z][a-zA-Z0-9]+)","Declaração ou invocação de funções permitida na linguagem."],
+        ["define_funcao","define_funcao","define_funcao","alavra reservada para a criação de funções dentro da linguagem."],
+        ["valor_logico","Verdadeiro,Falso","Verdadeiro|Falso","Valor booleano de falsidade ou negativo ou vazio a ser exibido, processado ou armazenado em uma variável do tipo ~booleano."],
+        ["valor_texto",'“Olá”, “Seu nome”, “Nome do Carro”, “Placa do Carro”, etc.','"[a-zA-Z0-9áàâãéèêíïóôõöúçÁÀ ÃÉÈÍÏÓÔÕÖÚ\.\s\_,\?!@\#$%&\|\*\(\)]*"',"Valor de qualquer texto a ser exibido, processado ou armazenado em uma variável do tipo ~texto."],
+        ["valor_inteiro","10, 1, 0, -3, 1239123, etc.","\b(\d+)\b","Valor de qualquer número inteiro a ser exibido, processado ou armazenado em uma variável do tipo ~numero_inteiro."],
+        ["valor_real","0.5,0.674","(\d+)\.(\d+)","Valor de qualquer número real a ser exibido, processado ou armazenado em uma variável do tipo ~numero_real."],
+        ["valor_data","20/10/2010, etc","[0-3][0-9]\/[0-1][0-9]\/[0-9][0-9][0-9][0-9]","Valor de qualquer número real a ser exibido, processado ou armazenado em uma variável do tipo ~data."],
+        ["valor_horario","18:13, 08:45, etc","[0-9][0-9]:[0-9][0-9]","Valor de qualquer número real a ser exibido, processado ou armazenado em uma variável do tipo ~horario."],
+        ["abre_parent","(","\(","Identificador de abrir parênteses."],
+        ["fecha_parent",")","\)","Identificador de fechar parênteses."],
+        ["abre_chaves","{","\{","Identificador de abrir de chaves."],
+        ["fecha_chaves","}","\}","Identificador de fechar de chaves."],
+        ["abre_comentario","[","[","Identificador de abrir comentário."],
+        ["fecha_comentario","]","]","Identificador de fechar comentario."],
+        ["ponto_final",".","\.","Identificador de final de linha."],
+        ["virgula",",",",","Identificador de virgula no código"],
+        ["espaco"," ","\s","Identificador de espaço no código"]   
     ]
 
     layout = [[sg.Table(
         values=tokens_registrados,
         headings=cabecalho_tabela,
         auto_size_columns=True,
-        max_col_width=50,
+        max_col_width=40,
         justification='center',
-        num_rows=2,
+        num_rows=32,
         key="_TABELATOKENS_",
-        row_height=15
+        row_height=16
     )]]
     janela = sg.Window("Tabela de Tokens", layout=layout, resizable=True)
     event, values = janela.read()
@@ -260,11 +289,11 @@ while True:
     if event in (None, "Exit"):
         window.close()
         break
-    if event in (file_new, "n:78"):
+    if event in (menu_novo_arquivo, "n:78"):
         arquivo = novo_arquivo()
-    if event in (file_open, "o:79"):
+    if event in (menu_abrir_arquivo, "o:79"):
         arquivo = abrir_arquivo()
-    if event in (file_save, "s:83"):
+    if event in (menu_salvar_arquivo, "s:83"):
         salvar_arquivo(arquivo)
     if event in ("Save As",):
         arquivo = salvar_como()
